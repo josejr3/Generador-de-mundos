616e818d8dfa57953976e0b689660c17d92dfb45
josejr3 <gregorio.jr2@gmail.com>
2026-01-12 19:21:59 +0100

    p

--- mundos.js
+++ mundos.js
@@ -1,6 +1,10 @@
 
+
 let form =document.getElementById("datos");
 let container=document.getElementById("container");
+
+
+
 form.addEventListener("submit",(event)=>{
     event.preventDefault();
     let datos=new FormData(form);
@@ -9,12 +13,18 @@ form.addEventListener("submit",(event)=>{
     console.log("hola");
     //setTimeout(()=>{
     //    console.log("adios");
-          form.style="display:flex";
-   //     
-   // },2300);
+    //      form.style="display:flex";
+    //     
+    // },2300);
    let canvas=document.createElement("canvas");
+   canvas.width=datos.get("CamposMapSize");
+   canvas.height=datos.get("CamposMapSize");
+   console.log(container);
    container.appendChild(canvas);
-   
+   canvas.getContext("2d");
+
+
 });
+
 
 
